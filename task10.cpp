ccf0d964566ed56e2fa0fd62bf3bf70fea4d1723
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-29 21:12:31 +0300

    Create task10.cpp
    Add a solution of the task number 10

---
+++ task10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+		
+	int A, B;
+	cout << "Type the value of A";
+	cin >> A;
+	cout << "Type the value of B";
+	cin >> B;
+	if (A % 2 == 0 || B % 2 == 0) {
+		cout << "True" << B, A;
+	} else {
+		cout << "Wrong";
+	}
+}
